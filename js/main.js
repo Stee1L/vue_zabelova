13f57f36b9b253d04997aced065c232ff791a1f1
mac <superstart8016@gmail.com>
2024-02-14 16:53:45 +0700

    Я добавиль кнопки

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 let product = "Socks";
 
+
+
 let app = new Vue({
     el: '#app',
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
+    },
+
+
+
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 9,
@@ -13,14 +23,24 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+
+
 
 
 
