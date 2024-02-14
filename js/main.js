72e85d2b5297e915810c822b19d3f595ca21805e
mac <superstart8016@gmail.com>
2024-02-14 16:23:07 +0700

    Рендеринг списков

--- js/main.js
+++ js/main.js
@@ -10,6 +10,20 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 9,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
+
+
     }
 })
 
